99221d57d71d602b48aedeb7f6d23300d49d0cbe
Anastasija <nastya@gst.lv>
2021-05-03 18:38:13 +0300

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,9 +12,48 @@ public class Main {
         System.out.print("Enter how many times: ");
         int howManyTimes = Integer.parseInt(input.nextLine());
 
-        for (int i = 1; i <= howManyTimes ; i++) {
+        for (int i = 1; i <= howManyTimes; i++) {
             int result = number * i;
-            System.out.println( number + " * " + i + " = " + result);
+            System.out.println(number + " * " + i + " = " + result);
+        }
+        System.out.println();
+
+        System.out.println("Multiplication table by 10");
+        for (int k = 1; k <= 10; k++) {
+            for (int n = 1; n <= 10; n++) {
+                System.out.printf("%5d ", k * n);
+            }
+            System.out.println(" ");
+        }
+        System.out.println();
+
+
+        System.out.print("Enter first number (start number): ");
+        int first = Integer.parseInt(input.nextLine());
+
+        System.out.print("Enter second number (stop number): ");
+        int second = Integer.parseInt(input.nextLine());
+
+        System.out.print("X  |");
+
+        for (int j = first; j <= second; j++) {
+            System.out.printf("%5d ", j);
+        }
+        System.out.println();
+        for (int y = first-1; y <= second; y++) {
+            System.out.printf("%5s ", "-----");
+        }
+        System.out.println();
+
+
+
+        for (int g = first; g <= second; g++) {
+            System.out.print(g + "  |");
+            for (int f = first; f <= second; f++) {
+                System.out.printf("%5d ", g * f);
+            }
+            System.out.println(" ");
         }
     }
 }
+
--- src/main/java/MultiplicationTable.java
+++ src/main/java/MultiplicationTable.java
@@ -1,49 +1,55 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class MultiplicationTable {
-
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-
-        int[][] table = new int[10][10];
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table[i].length; j++) {
-                table[i][j] = (i + 1) * (j + 1);
-            }
-        }
-
-        for (int[] a: table) {
-            for (int i: a) {
-                System.out.print(i + "\t");
-            }
-            System.out.print("\n");
-        }
-        System.out.println();
-
-
-
-        System.out.print("Enter first number (start number): ");
-        int first = Integer.parseInt(input.nextLine());
-
-        System.out.print("Enter second number (stop number): ");
-        int second = Integer.parseInt(input.nextLine());
-
-        int[][] userTable = new int[second-first+1][second-first+1];
-        for (int i = 0; i < userTable.length; i++) {
-            for (int j = 0; j < userTable[i].length; j++) {
-                userTable[i][j] = (i + first) * (j + first);
-            }
-        }
-
-        for (int[] a: userTable) {
-            for (int i: a) {
-                System.out.print(i + "\t");
-            }
-            System.out.print("\n");
-
-        }
-    }
-}
-
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class MultiplicationTable {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner input = new Scanner(System.in);
+//
+//        int[][] table = new int[10][10];
+//        for (int i = 0; i < table.length; i++) {
+//            for (int j = 0; j < table[i].length; j++) {
+//                table[i][j] = (i + 1) * (j + 1);
+//            }
+//        }
+//
+//        for (int[] a: table) {
+//            for (int i: a) {
+//                System.out.print(i + "\t");
+//            }
+//            System.out.print("\n");
+//        }
+//        System.out.println();
+//
+//
+//
+//        System.out.print("Enter first number (start number): ");
+//        int first = Integer.parseInt(input.nextLine());
+//
+//        System.out.print("Enter second number (stop number): ");
+//        int second = Integer.parseInt(input.nextLine());
+//
+//        int[][] userTable = new int[second-first+1][second-first+1];
+//        for (int i = 0; i < userTable.length; i++) {
+//            for (int j = 0; j < userTable[i].length; j++) {
+//                userTable[i][j] = (i + first) * (j + first);
+//            }
+//        }
+//        for (int i = first; i <= second; i++) {
+//            System.out.print(i + "\t");
+//        }
+//        System.out.println("\n--------------------------------------");
+//
+//        for (int i = first; i <= second; i++) {
+//            System.out.println(i + "|" + "\t");
+//            for (int[] a: userTable) {
+//                for (int num: a) {
+//                    System.out.print(num + "\t");
+//                }
+//                System.out.print("\n");
+//            }
+//        }
+//    }
+//}
+//
